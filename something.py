673b30fd43334459264c18fd948e12979e3b1b39
Nick Usme <86114032+Nick0306@users.noreply.github.com>
2021-10-20 13:37:32 -0400

    BEEFED code

--- something.py
+++ something.py
@@ -1,3 +1,16 @@
-name1 = input("Give me your  name: ")
-name2 = input("Give me your BFFs name: ")
-print("Hello", name1, "and", name2, "!")
+import random
+
+fortunes = {
+1:"You will have a great week",
+2:"You will have an eh so good week",
+3:"You will have a terrible week"
+}
+randomNum = random.randint(1,3)
+name1 = input("Hello, what is your name? I will predict how your week will go: ")
+
+if(randomNum == 1):
+	print(fortunes[1])
+elif(randomNum == 2):
+	print(fortunes[2])
+else:
+	print(fortunes[3])
